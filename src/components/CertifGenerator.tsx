29daea81c70cda5c6caacda82f1d36ca737f735d
Irsyad Al Ghifary <100660393+irsyaddd@users.noreply.github.com>
2023-02-19 08:04:03 +0700

    Update CertifGenerator.tsx
    update anchor X on text

--- src/components/CertifGenerator.tsx
+++ src/components/CertifGenerator.tsx
@@ -17,17 +17,10 @@ export default function CertifGenerator() {
     const textWidth = certificate.getTextWidth(name);
     const x = 0;
     const y = 0;
-    certificate.addImage(
-      "./certif-template.png",
-      "JPEG",
-      x,
-      y,
-      centerWidth - textWidth / 2,
-      212
-    );
+    certificate.addImage("./certif-template.png", "JPEG", x, y, 300, 212);
     certificate.setTextColor(0, 0, 0);
     certificate.setFontSize(24);
-    certificate.text(name, 120, 88);
+    certificate.text(name, centerWidth - textWidth / 2, 88);
     const pdfDataUrl = certificate.output("dataurlstring");
     const pdfPreview = document.getElementById(
       "pdf-preview"
